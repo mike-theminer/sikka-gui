5f2fe03692266249e19e778f3f9f004329acdbcc
mike-theminer <miner.mike@yandex.com>
2018-01-02 20:22:42 +0800

    remove git submodule that points to intensecoin

---
+++ CryptoNoteWallet.cmake
@@ -0,0 +1,4 @@
+set(CN_PROJECT_NAME "Sikka")
+set(CN_CURRENCY_DISPLAY_NAME "Sikka")
+set(CN_CURRENCY_TICKER "Sikka")
+set(CN_VERSION 1.2.0)
---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+		//48c23319-42a5-7ef9-dd71-6577006aa042
+	//~ const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x8c, 0x40, 0x3d, 0x04, 0x8f, 0x82, 0x2c, 0x3d, 0x82, 0x5f, 0x2c, 0x1a, 0x44, 0x32, 0x7d, 0x82 } };
+	const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x48, 0xc2, 0x33, 0x19, 0x42, 0xa5, 0x7e, 0xf9, 0xdd, 0x71, 0x65, 0x77, 0x00, 0x6a, 0xa0, 0x42 } };
+	
+  //const static boost::uuids::uuid BYTECOIN_NETWORK = { { 0x11 ,0x10, 0x01, 0x11 , 0x11, 0x00 , 0x01, 0x01, 0x10, 0x11, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10} }; //Bender's nightmare
+}
